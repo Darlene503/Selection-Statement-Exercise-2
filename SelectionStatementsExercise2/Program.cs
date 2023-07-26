ae8d6d27ed38b2973be1d305cbf9ca0141d4c05f
darlene503 <duyn503@gmail.com>
2023-07-26 15:20:54 -0700

    edit lowercse

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("What is your favorite subject?");
+            string subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "science":
+                    Console.WriteLine("science is cool!");
+                    break;
+                case "art":
+                    Console.WriteLine("art is the best!");
+                    break;
+                case "math":
+                    Console.WriteLine("math sucks!");
+                    break;
+                case "pe":
+                    Console.WriteLine("pe is fun!");
+                    break;
+                case "home economics":
+                    Console.WriteLine("home economics is boring");
+                    break;
+                default:
+                    Console.WriteLine("I wasn't expecting that.");
+                    break;
+
+
+
+            }
+
         }
     }
 }
